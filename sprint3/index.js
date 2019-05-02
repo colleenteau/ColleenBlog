7195c1b5f398f56cd9b3be9b52f6cab60eaa3aad
Colleen Te Au <oratech19@gmail.com>
2019-05-03 10:44:26 +1200

    new learning

--- sprint3/index.js
+++ sprint3/index.js
@@ -1,8 +1,37 @@
 console.log ("Welcome to sanwiches 3!")
 
 
-name = "Colleen"       // string
-age = 40
-console.log (age)
+name = "Colleen"                                             // string
+age = 40                                                     // int
+isCool = true                                                  // bool
+children = ["chelsea", "cherise", "brianna", "lachie"]    //array
+
+//building an object//
+
+Colleen = {
+    name: name,
+    age: age,
+    isCool: isCool,
+    children: children
+}
+
+
+Fraser = {
+    name: name"Fraser",
+    age: 29,
+    isCool: true,
+    children: ["chelsea", "cherise", "brianna", "lachie"]
+}
+
+//what are functions//
+
+function printName(person) {
+    console.log(person.name)
+}
+
+
+printName (Colleen)
+
+
 
 
